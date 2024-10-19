4ee85fcf9fc6bfcb43da421fe254fb6697dddf1d
Deskech <jose.manuellinan@hotmail.com>
2024-10-19 13:37:02 -0500

    Deleted the total feature for a replaced it for a new one

--- src/main/java/com/isadent/cotizaciones/microservicio/Domain/BussinesLogic/TotalPriceProcedures.java
+++
@@ -1,6 +0,0 @@
-package com.isadent.cotizaciones.microservicio.Domain.BussinesLogic;
-
-public interface TotalPriceProcedures {
-
-    Double procedimietoTotal(Double precio, Double abono); // soon to be deleted
-}
--- src/main/java/com/isadent/cotizaciones/microservicio/Infraestructure/Services/NewIsadentQuotationImp.java
+++ src/main/java/com/isadent/cotizaciones/microservicio/Infraestructure/Services/NewIsadentQuotationImp.java
@@ -2,7 +2,6 @@ package com.isadent.cotizaciones.microservicio.Infraestructure.Services;
 
 import com.isadent.cotizaciones.microservicio.Application.Dtos.NewQuotation;
 import com.isadent.cotizaciones.microservicio.Domain.BussinesLogic.NewIsadentQuotation;
-import com.isadent.cotizaciones.microservicio.Domain.BussinesLogic.TotalPriceProcedures;
 import com.isadent.cotizaciones.microservicio.Domain.Events.PublishPayment;
 import com.isadent.cotizaciones.microservicio.Domain.Events.PublishIsadentQuotation;
 import com.isadent.cotizaciones.microservicio.Domain.Model.Payment;
@@ -20,18 +19,15 @@ import java.util.List;
 public class NewIsadentQuotationImp implements NewIsadentQuotation<NewQuotation> {
     private final CurrencyFormat currencyFormat;
     private final QuotationDate quotationDate;
-    private final TotalPriceProcedures totalPriceProcedures;
     private final ImprovementPlanRepository<NewQuotation> improvementPlanRepository;
     private final PublishPayment publishPayment;
     private final PublishIsadentQuotation<PublishedQuotation> publishIsadentQuotation;
 
     public NewIsadentQuotationImp(CurrencyFormat currencyFormat, QuotationDate quotationDate,
-                                  TotalPriceProcedures totalPriceProcedures,
                                   ImprovementPlanRepository<NewQuotation> planDeMejoraJpaRepository,
                                   PublishPayment publishPayment, PublishIsadentQuotation<PublishedQuotation> publishIsadentQuotation) {
         this.currencyFormat = currencyFormat;
         this.quotationDate = quotationDate;
-        this.totalPriceProcedures = totalPriceProcedures;
         this.improvementPlanRepository = planDeMejoraJpaRepository;
         this.publishPayment = publishPayment;
         this.publishIsadentQuotation = publishIsadentQuotation;
@@ -45,20 +41,26 @@ public class NewIsadentQuotationImp implements NewIsadentQuotation<NewQuotation>
      */
     @Override
     public IsadentQuotation createNewIsadentQuotation(NewQuotation newQuotation) {
+
         //Set the payment money format
         String abonoCurrency = currencyFormat.createMoneyFormat(newQuotation.getPayment());
+
         // consults the dental procedures prices in the database
         List<ImprovementPlan> improvementPlan = improvementPlanRepository.getImprovementPlan(newQuotation);
+
         //calculate the Quotation's total amount
         Double totalPrice = improvementPlan.stream().map(ImprovementPlan::getPrice).reduce(0.0, Double::sum);
-        Double total = totalPriceProcedures.procedimietoTotal(totalPrice, newQuotation.getPayment());
+
         // Create the quotation date
         String date = quotationDate.createQuotationDate();
+
         // create the money format for the total
-        String totalCurrency = currencyFormat.createMoneyFormat(total);
+        String totalCurrency = currencyFormat.createMoneyFormat(totalPrice);
+
         // instance a new Payment to send it to the Payment microservice
         Payment paymentToSend = new Payment(newQuotation.getPatientName(), newQuotation.getPayment());
         publishPayment.publishNewPayment(paymentToSend);
+
         // when the information needed is ready we create the view for the Quotation
         IsadentQuotation isadent = new IsadentQuotation();
         isadent.setPatientName(newQuotation.getPatientName());
@@ -67,8 +69,9 @@ public class NewIsadentQuotationImp implements NewIsadentQuotation<NewQuotation>
         isadent.setTotal(totalCurrency);
         isadent.setPayment(abonoCurrency);
         PublishedQuotation publishedQuotation = new PublishedQuotation(improvementPlan, newQuotation.getPayment(), newQuotation.getPatientName(),
-                total,date
+                totalPrice,date
                 );
+
         //publish the event to the Bill microservice
         publishIsadentQuotation.publishQuotation(publishedQuotation);
         return isadent;
